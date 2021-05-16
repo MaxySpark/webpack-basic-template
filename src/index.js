36824d53a37829d041a1f69f5f06aa7d7bb8f366
MaxySpark <me@maxyspark.me>
2021-05-16 20:06:45 +0530

    basic webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { init } from './lib/init';
+import './styles/main.scss';
+
+init();
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCSSExtractlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+}
